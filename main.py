0f272bba3beb3edcd77e6952282c2fc66647fd5a
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-01-06 13:47:24 -0200

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from libs import iteration
 from libs import convert_to_text
 
 def main():
+    # read the image.
     im = Image.open('sample.jpeg')
+    # applying the filter.
     im = iteration.iterate(im, 2)
 
     # comment it out to see the actual processed image
     im.show()
-
+    
+    # convert text of image to string.
     convert_to_text.get_string_from_image(im)
     text = convert_to_text.get_string_from_image(im)
     print(text)
